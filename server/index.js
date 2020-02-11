3f833ed4a308e6988d3d7cf5c4b502a40fb2e2c4
Alex Kim <alexkim612@gmail.com>
2020-02-10 19:49:09 -0800

    compression added

--- server/index.js
+++ server/index.js
@@ -3,10 +3,14 @@ const app = express()
 const port = 9000
 const path = require('path');
 const morgan = require('morgan');
+var compression = require('compression');
 const router = require('./router.js');
 
 //serve files from dist
 app.use(express.static(path.join(__dirname, '../client/dist')));
+
+// compress files coming in
+app.use(compression());
 
 //See all incoming requests
 app.use(morgan('dev'));
